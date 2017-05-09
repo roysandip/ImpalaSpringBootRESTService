8aa20211b1d8fbbb2a27444b3f525d005f88fe02
Sandip Roy <sandip.roy@hotmail.com>
2017-05-09 08:46:29 -0400

    Create ImpalaSpringBootApplication.java

---
+++ src/main/java/com/mycompany/bigdata/ImpalaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpalaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(new Object[]{ImpalaSpringBootApplication.class}, args);
+	}
+}
